832218b7ace0249d40a5588157a36edd1cf46f95
padawanr0k <padawanr0k@gmail.com>
2018-03-22 23:40:50 +0900

    add tooltip directive

--- V5/src/app/tooltip.directive.ts
+++ V5/src/app/tooltip.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 export class TooltipDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
   
-  text = this.renderer.createText('bottom');
+  text = this.renderer.createText('😄');
   tooltip = this.renderer.createElement('div');
   hieght: number;
   tooltipElement: object;
@@ -24,24 +24,27 @@ export class TooltipDirective {
     this.renderer.appendChild(this.el.nativeElement, this.tooltip);
     
     this.tooltipElement = this.el.nativeElement.children[0].getBoundingClientRect();
-    this.tooltipWidth = this.tooltipElement.width;;
+    this.tooltipWidth = this.tooltipElement.width;
     this.tooltipheigt = this.tooltipElement.height;
     console.log(this.tooltipElement)
     console.dir(this.renderer);
 
     if ( this.el.nativeElement.classList.contains('tooltip-bottom') ) {
       this.renderer.setStyle(this.tooltip, 'margin-left', `${(-this.tooltipWidth / 2).toString()}px`);
-      this.renderer.setStyle(this.tooltip, 'bottom', `${(-this.tooltipheigt)}px`);
-      this.tooltipElement.x = 1000;
+      this.renderer.setStyle(this.tooltip, 'left', '50%');
+      this.renderer.setStyle(this.tooltip, 'bottom', `-${(this.tooltipheigt)+10}px`);
     } else if ( this.el.nativeElement.classList.contains('tooltip-top') ) {
       this.renderer.setStyle(this.tooltip, 'margin-left', `${(-this.tooltipWidth / 2).toString()}px`);
-      this.renderer.setStyle(this.tooltip, 'top', `-${this.tooltipheigt}px`);
+      this.renderer.setStyle(this.tooltip, 'left', '50%');
+      this.renderer.setStyle(this.tooltip, 'top', `-${this.tooltipheigt+10}px`);
     } else if ( this.el.nativeElement.classList.contains('tooltip-left') ) {
-      this.renderer.setStyle(this.tooltip, 'margin-left', `${(-this.tooltipWidth / 2).toString()}px`);
-      this.renderer.setStyle(this.tooltip, 'top', `-${this.tooltipheigt}px`);
+      this.renderer.setStyle(this.tooltip, 'margin-top', `${(-this.tooltipheigt / 2).toString()}px`);
+      this.renderer.setStyle(this.tooltip, 'top', '50%');
+      this.renderer.setStyle(this.tooltip, 'left', `-${this.tooltipWidth+10}px`);
     } else if ( this.el.nativeElement.classList.contains('tooltip-right') ) {
-      this.renderer.setStyle(this.tooltip, 'margin-left', `${(-this.tooltipWidth / 2).toString()}px`);
-      this.renderer.setStyle(this.tooltip, 'top', `-${this.tooltipheigt}px`);
+      this.renderer.setStyle(this.tooltip, 'margin-top', `${(-this.tooltipheigt / 2).toString()}px`);
+      this.renderer.setStyle(this.tooltip, 'top', '50%');
+      this.renderer.setStyle(this.tooltip, 'right', `-${this.tooltipWidth+10}px`);
     } 
   }
   // @HostListener('mouseleave')
